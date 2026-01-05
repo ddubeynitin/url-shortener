2a768b0e82d948679446a43d2d358d2a09acdd56
Nitin Dubey <ddubeynitin30@gmail.com>
2026-01-06 00:30:47 +0530

    Fix the CORS error

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const urlRoute = require("./routes/url");
 const { connectDB } = require("./connect");
 const URL = require("./models/url");
 
 const app = express();
 dotenv.config();
+
 const PORT = process.env.PORT || 3000;
 const MONGODBURL = process.env.MONGODBURL;
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  optionsSuccessStatus: 200,
+};  
 
 connectDB(MONGODBURL)
   .then(() => console.log("Connected to MongoDB"))
@@ -15,7 +22,11 @@ connectDB(MONGODBURL)
 
 app.use(express.json());
 
-app.use("/url", urlRoute); // Use the url routes for /url endpoint
+console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
+
+app.use(cors(corsOptions)); // Enable CORS for all routes (development)
+
+app.use("/url", cors(corsOptions), urlRoute); // Use the url routes for /url endpoint
 
 
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
